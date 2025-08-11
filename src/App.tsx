ea711e13e43ea1c50c3b945b34d6ec1824b4be8b
Usman Khalid <ukhalid428@gmail.com>
2025-08-11 21:37:52 +0100

    Setting up Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Container, Grid } from "@mui/material";
 import TopNav from "./components/TopNav";
 
 export default function App() {
-  return <TopNav />;
+  return (
+    <Container maxWidth="xl">
+      <TopNav />
+      <Grid container spacing={4} mt={8} sx={{ height: "100vh" }}>
+        <Grid
+          size={6}
+          sx={{ border: "1px solid black", height: "100%" }}
+        ></Grid>
+        <Grid
+          size={6}
+          sx={{ border: "1px solid black", height: "100%" }}
+        ></Grid>
+      </Grid>
+    </Container>
+  );
 }
